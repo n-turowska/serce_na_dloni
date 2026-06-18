d21acc8c89fa14effc22067120d8c07ded071830
Ula Twardowska <utwardowska@student.agh.edu.pl>
2026-06-18 13:04:35 +0200

    dodanie toMap i fromMap

--- lib/models/pressure_entry.dart
+++ lib/models/pressure_entry.dart
@@ -32,6 +32,40 @@ class PressureEntry {
     );
   }
 
+  // ===========================================================================
+  // TODO 1: Implement toMap() and fromMap() for SQLite serialization
+  
+  Map<String, dynamic> toMap() {
+    return{
+      'id': id,
+      'systolic': systolic,
+      'diastolic': diastolic,
+      'note': note,
+      'created_at': createdAt.toIso8601String(),
+    };
+  }
+  
+  factory PressureEntry.fromMap(Map<String, dynamic> map) {
+    return PressureEntry(
+      id: map['id'] as String,
+      systolic: map['systolic'] as int,
+      diastolic: map['diastolic'] as int,
+      note: map['note'] as String?,
+      createdAt: DateTime.parse(map['created_at'] as String),
+    );
+  }
+
+  // TODO 1: Uncomment and complete:
+  //
+  // Map<String, dynamic> toMap() {
+  //   return {};
+  // }
+  //
+  // factory MoodEntry.fromMap(Map<String, dynamic> map) {
+  //   return MoodEntry(score: 0); // Replace with actual implementation
+  // }
+
+
   @override
   String toString() {
     return 'PressureEntry(id: $id, systolic: $systolic, diastolic: $diastolic, note: $note, createdAt: $createdAt)';
