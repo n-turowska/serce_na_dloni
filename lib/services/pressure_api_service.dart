e94cd0753e3a8090d09cb5f7413bf1be8ba6d26d
Nina Turowska <nturowska@student.agh.edu.pl>
2026-06-19 11:48:01 +0200

    implementacja get pressures w pressure api service

--- lib/services/pressure_api_service.dart
+++ lib/services/pressure_api_service.dart
@@ -7,6 +7,14 @@ class PressureApiService {
   PressureApiService({ApiClient? apiClient})
       : _apiClient = apiClient ?? ApiClient();
 
+  Future<List<PressureEntry>> getPressures() async {
+    final data = await _apiClient.get('/pressures'); 
+    final entries = data['entries'] as List; 
+    return entries
+       .map((e) => PressureEntry.fromJson(e as Map<String, dynamic>))
+       .toList();
+  }
+
 
 
 }
